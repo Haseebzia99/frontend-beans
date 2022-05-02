e344faeb6d20483fdc9e5086707667c8a5c7fa9b
Haseeb Zia <haseeb.zia@asos.com>
2022-05-02 01:11:39 +0100

    pckg.json revert

--- app/src/app/children/comments/__tests__/comments.test.js
+++ app/src/app/children/comments/__tests__/comments.test.js
@@ -3,7 +3,6 @@ import { Comments } from "../index";
 import { render, screen} from '@testing-library/dom';
 import data from '../../data.json';
 
-
 describe("Comments", () => {
   it("should render Comments correctly passing data", () => {
     render(<Comments data={data} />);
